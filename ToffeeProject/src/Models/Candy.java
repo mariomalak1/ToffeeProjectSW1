a254c112a7746d134514336b3003d33993ace888
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-05-04 16:14:32 +0300

    adding Candy class

--- ToffeeProject/src/Models/Candy.java
+++ ToffeeProject/src/Models/Candy.java
@@ -1,5 +1,116 @@
 package Models;
+import java.time.DateTimeException;
+
+class Unit{
+    private int ID;
+    public String unitName;
+    private static int LastID = 0;
+    Unit(String name){
+        LastID++;
+        ID = LastID;
+        unitName = name;
+    }
+}
 
 public class Candy{
+    private int ID;
+    private String Name;
+    private float Price;
+    private Unit Unit;
+    private int CategoryID;
+    private int LoyaltyPoints;
+    private int AdminIDCreatedBY;
+    private DateTimeException TimeCreatedAt;
+    private String Description;
+    private String ImagePath;
+    private static int LastID = 0;
 
+    Candy(String name, float price, String unitName, int Category, int loyaltyPoints, int IDCreatedBy, String description, String image){
+        LastID++;
+        ID = LastID;
+        this.Name = name;
+        this.Price = price;
+        this.CategoryID = Category;
+        this.LoyaltyPoints = loyaltyPoints;
+        this.AdminIDCreatedBY = IDCreatedBy;
+        this.Description = description;
+        this.ImagePath = image;
+        Models.Unit unit = new Unit(unitName);
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.CategoryID = categoryID;
+    }
+
+    public int getCategoryID() {
+        return this.CategoryID;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public float getPrice() {
+        return Price;
+    }
+
+    public void setPrice(float price) {
+        Price = price;
+    }
+
+    public int getLoyaltyPoints() {
+        return LoyaltyPoints;
+    }
+
+    public void setLoyaltyPoints(int loyaltyPoints) {
+        LoyaltyPoints = loyaltyPoints;
+    }
+
+    public int getAdminIDCreatedBY() {
+        return AdminIDCreatedBY;
+    }
+
+    public void setAdminIDCreatedBY(int adminIDCreatedBY) {
+        AdminIDCreatedBY = adminIDCreatedBY;
+    }
+
+    public DateTimeException getTimeCreatedAt() {
+        return TimeCreatedAt;
+    }
+
+    public void setTimeCreatedAt(DateTimeException timeCreatedAt) {
+        TimeCreatedAt = timeCreatedAt;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getImagePath() {
+        return ImagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        ImagePath = imagePath;
+    }
+
+    public String getUnit() {
+        return Unit.unitName;
+    }
+
+    public void setUnit(String unitName) {
+        Models.Unit unit = new Unit(unitName);
+    }
 }
